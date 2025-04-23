713a0a648cd32e4e8641a6047d92fcd005f57665
partic2 <1596067968@qq.com>
2025-04-23 10:47:16 +0800

    add template query for abstract database

--- source/pxseedServer2023/db/base.ts
+++ source/pxseedServer2023/db/base.ts
@@ -1,7 +1,7 @@
 
 
 
-import {future} from 'partic2/jsutils1/base'
+import {assert, future} from 'partic2/jsutils1/base'
 
 export interface DatabaseConnectOption{
     host?:string,
@@ -13,11 +13,26 @@ export interface DatabaseConnectOption{
     withColumnsInfo?:boolean
 }
 
-export interface sqlDb{
-    setConnectionInfo(opt:DatabaseConnectOption):sqlDb
-    ensureConnected():Promise<'ok'|'connected'>;
-    queryToArray(sql:string):Promise<{rows:(string|number|Date)[][],fields:string[],columnsInfo?:{type?:string}[]}>;
-    queryToMap(sql:string):Promise<{rows:{[field:string]:(string|number|Date)}[],columnsInfo?:{type?:string}[]}>;
-    execSql(sql:string): Promise<{ insertId?: number | undefined; affectedRows?: number }>;
+export abstract class sqlDb{
+    abstract setConnectionInfo(opt:DatabaseConnectOption):sqlDb
+    abstract ensureConnected():Promise<'ok'|'connected'>;
+    queryToArray(sql:string):Promise<{rows:(string|number|Date)[][],fields:string[],columnsInfo?:{type?:string}[]}>{
+        let arr=[sql] as Array<string>&{raw:Array<string>};
+        arr.raw=arr;
+        return this.queryToArrayTpl!(arr)
+    }
+    abstract queryToArrayTpl(sql:TemplateStringsArray,...args:any[]):Promise<{rows:(string|number|Date)[][],fields:string[],columnsInfo?:{type?:string}[]}>;
+    queryToMap(sql:string):Promise<{rows:{[field:string]:(string|number|Date)}[],columnsInfo?:{type?:string}[]}>{
+        let arr=[sql] as Array<string>&{raw:Array<string>};
+        arr.raw=arr;
+        return this.queryToMapTpl!(arr)
+    };
+    abstract queryToMapTpl(sql:TemplateStringsArray,...args:any[]):Promise<{rows:{[field:string]:(string|number|Date)}[],columnsInfo?:{type?:string}[]}>;
+    execSql(sql:string): Promise<{ insertId?: number | undefined; affectedRows?: number }>{
+        let arr=[sql] as Array<string>&{raw:Array<string>};
+        arr.raw=arr;
+        return this.execSqlTpl!(arr)
+    };
+    abstract execSqlTpl(sql:TemplateStringsArray,...args:any[]): Promise<{ insertId?: number | undefined; affectedRows?: number }>;
 }
 
